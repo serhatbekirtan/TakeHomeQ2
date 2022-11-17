30ad8dc8d5bc8b01fda557c7f6a2a07033b1369f
orfein <serhatbekirtan@gmail.com>
2022-11-17 14:39:54 +0300

    Commit 10K with non-distinct numbers solution.

--- src/com/Prog2/Main.java
+++ src/com/Prog2/Main.java
@@ -1,41 +1,51 @@
 package com.Prog2;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        //int[] arr = {3, 0, 1, 8, 7, 2, 5, 4, 9, 6};
-        int[] inputArray10k = new int[10000];
+        Integer[] arr = {3, 0, 1, 8, 7, 2, 4, 4, 9, 6};
+        /*int[] inputArray10k = new int[10000];
         for (int i = 0; i < inputArray10k.length; i++){
             inputArray10k[i] = i;
-        }
-        long start = System.currentTimeMillis();
-        HungarianQS(inputArray10k, 0, inputArray10k.length - 1);
-        long end = System.currentTimeMillis();
+        }*/
+        //long start = System.currentTimeMillis();
+        /*Integer[] arr = new Integer[1000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }*/
+        //Collections.shuffle(Arrays.asList(arr));
+        System.out.println(Arrays.toString(arr));
+        HungarianQS(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+        //long end = System.currentTimeMillis();
         //System.out.println("Sorted array: ");
         //printArray(inputArray10k, inputArray10k.length);
-        System.out.println((end - start) + " ms");
+        //System.out.println((end - start) + " ms");
     }
 
-    public static int partition(int[] arr, int low, int high){
+    public static int partition(Integer[] arr, int low, int high){
         int pivot = arr[low];
         int i = low;
         int j = high;
         int temp;
         while (true){
-            while (arr[j] > pivot){
+            while (arr[j] >= pivot && j > 0){
                 j--;
             }
 
             if (i >= j){
-                return j;
+                return i;
             }
 
             temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
 
-            while (arr[i] < pivot){
+            while (arr[i] <= pivot && i < arr.length - 1){
                 i++;
             }
 
@@ -49,17 +59,11 @@ public class Main {
         }
     }
 
-    public static void HungarianQS(int[] arr, int low, int high){
+    public static void HungarianQS(Integer[] arr, int low, int high){
         if (low < high) {
             int i = partition(arr, low, high);
             HungarianQS(arr, low, i);
             HungarianQS(arr, i + 1, high);
         }
-    }
-
-    public static void printArray(int[] arr, int n){
-        for (int i = 0; i < n; i++)
-            System.out.print(" " + arr[i]);
-        System.out.println();
     }
 }
